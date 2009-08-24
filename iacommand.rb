9cda98be499bef46021f18f48adce1c9fe81faf2
Pito Salas <pito@Travel.local>
2009-08-24 16:13:02 -0400

    iadded some tweaks

---
+++ iacommand.rb
@@ -0,0 +1,74 @@
+#!/usr/bin/env ruby
+=begin
+  * Name: Ballot-analyzer
+  * Description: Analyze voting ballots
+  * Author: Pito Salas
+  * Copyright: (c) R. Pito Salas and Associates, Inc.
+  * Date: January 2009
+  * License: GPL
+
+  This file is part of Ballot-analyzer.
+
+  Ballot-analyzer is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Ballot-analyzer is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Ballot-analyzer.  If not, see <http://www.gnu.org/licenses/>.
+
+  require "ruby-debug"
+  Debugger.settings[:autolist] = 1 # list nearby lines on stop
+  Debugger.settings[:autoeval] = 1
+  Debugger.start
+=end
+
+require 'rubygems'
+require 'getoptlong'
+
+parser = GetoptLong.new
+parser.set_options(
+              ["-h", "--help", GetoptLong::NO_ARGUMENT],
+              ["-t", "--test", GetoptLong::NO_ARGUMENT],
+              ["-v", "--version", GetoptLong::NO_ARGUMENT])
+
+valid = false
+loop do
+  
+  begin
+    opt, arg = parser.get
+    break if not opt
+    case opt
+      when "-h"
+        puts "Usage: ..."
+        valid = true
+        break
+      when "-t"
+        valid = true
+        break
+      when "-v"
+        puts "Version 0.0"
+        valid = true
+        break
+      end
+    end
+  end
+
+if valid
+   puts "start"
+   i = 0
+    5.times do
+    puts "ballot #{i}"
+    delay = rand(5).to_i
+    sleep delay
+    i = i+1
+  end
+  puts "exit"
+else
+  puts "invalid parameters for iacommand"
+end
